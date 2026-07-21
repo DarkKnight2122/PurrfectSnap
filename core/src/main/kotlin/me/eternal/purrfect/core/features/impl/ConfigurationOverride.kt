501664d4eb56b339195d35b77578b74ec06ae351
DarkKnight2122 <145188583+DarkKnight2122@users.noreply.github.com>
2026-07-21 22:12:48 +0530

    fix: Refactor core initialization servers from URL blocklist

--- core/src/main/kotlin/me/eternal/purrfect/core/features/impl/ConfigurationOverride.kt
+++ core/src/main/kotlin/me/eternal/purrfect/core/features/impl/ConfigurationOverride.kt
@@ -75,7 +75,7 @@ class ConfigurationOverride : Feature("Configuration Override") {
             overrideProperty("SPONSORED_SNAPS_ENABLED", { context.config.global.blockAds.get() }, { false })
             overrideProperty("SPONSORED_SNAP_UPDATE_SPONSORED_FEED_ITEM", { context.config.global.blockAds.get() }, { false })
 
-            arrayOf("CUSTOM_AD_TRACKER_URL", "CUSTOM_AD_INIT_SERVER_URL", "CUSTOM_AD_SERVER_URL", "INIT_PRIMARY_URL", "INIT_SHADOW_URL", "GRAPHENE_HOST").forEach {
+            arrayOf("CUSTOM_AD_TRACKER_URL", "CUSTOM_AD_INIT_SERVER_URL", "CUSTOM_AD_SERVER_URL", "GRAPHENE_HOST").forEach {
                 overrideProperty(it, { context.config.global.blockAds.get() }, { "http://127.0.0.1" })
             }
 
